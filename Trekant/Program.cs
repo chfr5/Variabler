384cf4f4671c5f21609c2df3c0b3d0d710c34537
chfr5 <31880812+chfr5@users.noreply.github.com>
2017-09-12 16:09:50 +0200

    Vare og trekant

---
+++ Trekant/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trekant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangle d = new triangle(25, 10);
+            Console.WriteLine(d.Grundlinie.ToString() + '.' + d.Højde.ToString());
+            Console.WriteLine(d.Areal);
+        }
+    }
+    class triangle
+    {
+        public int Grundlinie { get; private set; }
+        public int Højde { get; private set; }
+
+        public triangle(int grundlinie, int højde)
+        {
+            this.Grundlinie = grundlinie;
+            this.Højde = højde;
+        }
+        public double Areal
+        {
+            get
+            {
+                return this.Grundlinie * this.Højde * 0.5;
+            }
+        }
+    }
+}
+
---
+++ Vare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Vare
+    {
+        public Vare()
+        {
+
+        }
+        public Vare(string navn, double pris)
+        {
+            
+        }
+        private string navn;
+
+        public string Navn
+        {
+            get { return navn; }
+            set { navn = value; }
+        }
+
+
+    }
+}
