8db26d02c7a7a0902383817d25389353b4e5c2f5
chfr5 <31880812+chfr5@users.noreply.github.com>
2017-09-13 09:08:24 +0200

    nu med vare

--- Vare/Program.cs
+++ Vare/Program.cs
@@ -10,6 +10,12 @@ namespace Vare
     {
         static void Main(string[] args)
         {
+            Vare v = new Vare();
+            v.Navn = "vare #1";
+            v.Pris = 100;
+            Console.WriteLine("Vare: " + v.Navn + " koster " + v.PrisMedMoms() + "kr inkl moms");
+            Vare v2 = new Vare("vare #2", 200);
+            Console.WriteLine(v2.PrisMedMoms());
         }
     }
     public class Vare
@@ -20,16 +26,35 @@ namespace Vare
         }
         public Vare(string navn, double pris)
         {
-            
+            this.Navn = navn;
+            this.Pris = pris;
         }
         private string navn;
 
         public string Navn
         {
-            get { return navn; }
-            set { navn = value; }
+            get {
+                Console.WriteLine("Nu aflæses navn:" + navn);
+                return navn; }
+            set {
+                Console.WriteLine("Nu sættes navn:" + value);
+                navn = value; }
+        }
+        private double pris;
+
+        public double Pris
+        {
+            get {
+                Console.WriteLine("Nu aflæses pris:" );
+                return pris; }
+            set {
+                Console.WriteLine("Nu tildeles pris: " + value + " til: " + navn);
+                pris = value; }
         }
 
-
+        public double PrisMedMoms()
+        {
+            return this.pris * 1.25;
+        }
     }
 }
