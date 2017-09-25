a7b778b5a6b8837ef667618ff5ee16b1d0a4caf3
Christian Frederiksen <chfr@lb.dk>
2017-09-25 15:48:01 +0200

    modul 11

---
+++ Module11_Collection/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module11_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bunke b = new Bunke();
+            return;
+            List<Person> list = new List<Module11_Collection.Person>();
+            list.Add(new Person() { Id = 10, Navn = "Christian" });
+            list.Add(new Person() { Id = 20, Navn = "Kirstine" });
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Navn);
+            }
+            Dictionary<int, Person> list2 = new Dictionary<int, Person>();
+            list2.Add(10, new Person() { Id = 10, Navn = "Christian" });
+            list2.Add(20, new Person() { Id = 20, Navn = "Kirstine" });
+            list2.Add(30, new Person() { Id = 21, Navn = "Carohline" });
+            var count = list2.Count;
+            Console.WriteLine("antal "+ count );
+            var l = list2[10];
+            Console.WriteLine(l.Navn);
+            return;
+            minetyper<int> i = new minetyper<int>();
+        }
+    }
+    public class Kort
+    {
+        public string Kulør { get; set; }
+        public int Værdi { get; set; }
+        public override string ToString()
+        {
+            //return base.ToString();
+            return this.Kulør + " " + this.Værdi;
+        }
+    }
+    /// <summary>
+    /// dette er en Person
+    /// </summary>
+    /// <typeparam name="t"></typeparam>
+    class Person
+    {   
+        /// <summary>
+        /// Id er nøgle
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Navn består af for og efternavn
+        /// </summary>
+        public string Navn { get; set; }
+    }
+    class minetyper<T>
+    {
+        T temp;
+
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T tpm = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
---
+++ Module11_generic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module11_generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = "";
+            ip = System.Configuration.ConfigurationManager.AppSettings["ip"];
+
+            ip = GetAppSetting("ip", "123");
+            int MaxRcd = GetAppSettingGen("maxRecords", 12);
+
+        }
+        static string GetAppSetting(string nøgle, string ellersVærdi)
+        {
+            string x = System.Configuration.ConfigurationManager.AppSettings[nøgle];
+            if (x == null)
+                return ellersVærdi;
+            return x; 
+        }
+        static T GetAppSettingGen<T>(string nøgle, T ellersVærdi)
+        {
+            string x = System.Configuration.ConfigurationManager.AppSettings[nøgle];
+            if (x == null)
+                return ellersVærdi;
+            return (T)Convert.ChangeType(x, typeof(T));
+        }
+    }
+}
